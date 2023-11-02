47a7d7c4a91d2221d0517c5780cd5b9dfb7393bc
Luka Gobovic <lukagobovic@gmail.com>
2023-11-02 15:11:11 -0400

    working on lab3

--- Lab3/lab3.py
+++ Lab3/lab3.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import time
+
 
 class encoder:
     encoder = nn.Sequential(
@@ -42,64 +45,91 @@ class encoder:
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("Using device:", device)
 
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(),  # Randomly flip images horizontally
+    transforms.RandomCrop(32, padding=4),  # Randomly crop images with padding
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
 
 train_dataset = datasets.CIFAR100(root='./data', train=True, download=True, transform=transform)
 test_dataset = datasets.CIFAR100(root='./data', train=False, download=True, transform=transform)
 
-train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, pin_memory=True)
-test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False, pin_memory=True)
-
-
-# Step 2: Create a custom frontend for classification
-class CustomFrontend(nn.Module):
-    def __init__(self, num_classes):
-        super(CustomFrontend, self).__init__()
-        # Add your custom layers here, e.g., fully connected layers
-        self.fc1 = nn.Linear(512, 256)
-        self.fc2 = nn.Linear(256, num_classes)
-
-    def forward(self, x):
-        x = x.view(x.size(0), -1)  # Flatten the input
-        x = self.fc1(x)
-        x = self.fc2(x)
-        return x
-
+train_loader = DataLoader(train_dataset, batch_size=256, shuffle=True, pin_memory=True)
+test_loader = DataLoader(test_dataset, batch_size=256, shuffle=False, pin_memory=True)
 
 class VGGCustomClassifier(nn.Module):
     def __init__(self, num_classes):
         super(VGGCustomClassifier, self).__init__()
-        self.vgg_encoder = encoder
-        self.custom_frontend = CustomFrontend(num_classes)
+        self.vgg_encoder = encoder.encoder
+        self.fc1 = nn.Linear(8192, 256)  # Adjusted for your specific VGG configuration
+        self.dropout1 = nn.Dropout(0.5)  # Add dropout with a probability of 0.5
+        self.fc2 = nn.Linear(256, num_classes)
+        self.dropout2 = nn.Dropout(0.3)
 
     def forward(self, x):
         x = self.vgg_encoder(x)
-        x = self.custom_frontend(x)
+        x = x.view(x.size(0), -1)  # Flatten the input
+        x = self.fc1(x)
+        x = self.dropout1(x)
+        x = self.fc2(x)
+        x = self.dropout2(x)
         return x
 
-# Step 4: Train the model on the CIFAR-100 dataset
-def train(model, train_loader, criterion, optimizer, num_epochs):
+# Train the model
+def train(model, train_loader, criterion, optimizer, num_epochs, scheduler):
     model.train()
+    losses = [] 
+    start_time = time.time()  # Start the timer
     for epoch in range(num_epochs):
         running_loss = 0.0
         for inputs, labels in train_loader:
+            inputs, labels = inputs.to(device), labels.to(device)  # Move data to the GPU
             optimizer.zero_grad()
             outputs = model(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
+        losses.append(running_loss / len(train_loader))
+        scheduler.step(running_loss)
         print(f"Epoch {epoch + 1}, Loss: {running_loss / len(train_loader)}")
 
-# Initialize the model and training parameters
+    end_time = time.time()  # End the timer
+    total_time = end_time - start_time
+    print(f"Training completed in {total_time/60.0:.2f} minutes.")
+
+    plt.plot(range(1, num_epochs + 1), losses)
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Training Loss')
+    plt.savefig('CIFAR100B256')
+    plt.show()
+
 num_classes = 100  # Number of classes in CIFAR-100
+
+# Load the pre-trained encoder model
+encoder_path = 'encoder.pth'  # Replace with the path to your pre-trained encoder model
+
+# Initialize your model with the pre-trained encoder's architecture
 model = VGGCustomClassifier(num_classes).to(device)
+
+# Load the pre-trained weights into your model's encoder
+model.vgg_encoder.load_state_dict(torch.load(encoder_path))
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.0005, momentum=0.9)
-num_epochs = 10  # You can adjust this value
+optimizer = torch.optim.SGD(model.parameters(), lr=0.0001,momentum=0.9)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min')  
+num_epochs = 60 # You can adjust this value
 
 # Train the model
-train(model, train_loader, criterion, optimizer, num_epochs)
+train(model, train_loader, criterion, optimizer, num_epochs, scheduler)
+
+torch.save(model.state_dict(), 'CIFAR100B256.pth')
+
+# Load the trained model for evaluation
+model = VGGCustomClassifier(num_classes).to(device)
+model.load_state_dict(torch.load('CIFAR100B256.pth'))
+model.eval()
 
 # Step 5: Evaluate the model and calculate top-1 and top-5 error rates
 def evaluate(model, test_loader):
@@ -108,8 +138,11 @@ def evaluate(model, test_loader):
     correct_top5 = 0
     total = 0
 
+    #with torch.no_d各而寧配额貴。
+    
     with torch.no_grad():
         for inputs, labels in test_loader:
+            inputs, labels = inputs.to(device), labels.to(device)  # Move data to the GPU
             outputs = model(inputs)
             _, predicted = outputs.max(1)
             total += labels.size(0)
@@ -121,8 +154,13 @@ def evaluate(model, test_loader):
 
     top1_error = 1 - correct_top1 / total
     top5_error = 1 - correct_top5 / total
+    top1_accuracy = correct_top1 / total
+    top5_accuracy = correct_top5 / total
+    
 
     print(f"Top-1 Error: {top1_error * 100:.2f}%")
     print(f"Top-5 Error: {top5_error * 100:.2f}%")
+    print(f"Top-1 Accuracy: {top1_accuracy * 100:.2f}%")
+    print(f"Top-5 Accuracy: {top5_accuracy * 100:.2f}%")
 
 evaluate(model, test_loader)
